dee2f5ffac0a69757e8b128690404c8bfbdc2b2c
Ahmet Yıldırım <ahmet.selman.yildirimm@gmail.com>
2023-11-10 02:46:48 +0300

    feat: create todo

--- web-todo/src/components/Input.tsx
+++ web-todo/src/components/Input.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 function Input() {
+  const [todo, setTodo] = useState("");
+
+  const addNewTodo = () => {
+    if (todo) {
+      axios.post("http://localhost:4000/api/todo", {
+        title: todo,
+        completed: false,
+      });
+      window.location.reload();
+    } else {
+      alert("task cannot be empty!!!");
+    }
+  };
+
   return (
     <div>
       <h2>TodoInput</h2>
@@ -8,9 +23,16 @@ function Input() {
           <svg style={{ width: 30 }} fill={"blue"} viewBox="0 0 448 512">
             <path d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
           </svg>
-          <input style={{ width: "100%" }} type="text" placeholder="New Todo" />
+          <input
+            style={{ width: "100%" }}
+            type="text"
+            placeholder="New Todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
         </div>
         <div
+          onClick={() => addNewTodo()}
           style={{
             width: "100%",
             marginTop: 30,
